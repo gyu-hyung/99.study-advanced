29be00cd0266691397d80d12510cf4af151bc386
gyu-hyung <94032808+gyu-hyung@users.noreply.github.com>
2023-04-11 21:36:22 +0900

    add

---
+++ src/main/java/hello/advanced/vo0/OrderServiceV0.java
@@ -0,0 +1,17 @@
+package hello.advanced.vo0;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class OrderServiceV0 {
+
+    private final OrderRepositoryV0 orderRepositoryVO;
+
+
+    public void orderItem(String itemId){
+        orderRepositoryVO.save(itemId);
+    }
+
+}
